e90416f864f3ec26617b0564dbbfd74f87550a06
ivadsiuls <ivadsiuls@proton.me>
2026-07-10 00:37:33 -0300

    refactor page, new themes, better snippets, and Duckable mode. (v3.0.0

--- scripts/clean.ps1
+++ scripts/clean.ps1
@@ -3,9 +3,7 @@ $ErrorActionPreference = "Stop"
 $projectRoot = Split-Path -Parent $PSScriptRoot
 
 Remove-Item -LiteralPath "$projectRoot\services.json" -Force -ErrorAction SilentlyContinue
-Remove-Item -LiteralPath "$projectRoot\import-plugin.rbxm" -Force -ErrorAction SilentlyContinue
-Remove-Item -LiteralPath "$projectRoot\build\import-plugin.rbxm" -Force -ErrorAction SilentlyContinue
-Remove-Item -LiteralPath "$projectRoot\build\import-plugin-dev.rbxm" -Force -ErrorAction SilentlyContinue
 Remove-Item -LiteralPath "$projectRoot\codekit-plugin.rbxm" -Force -ErrorAction SilentlyContinue
+Remove-Item -LiteralPath "$projectRoot\codekit-plugin-dev.rbxm" -Force -ErrorAction SilentlyContinue
 Remove-Item -LiteralPath "$projectRoot\build\codekit-plugin.rbxm" -Force -ErrorAction SilentlyContinue
 Remove-Item -LiteralPath "$projectRoot\build\codekit-plugin-dev.rbxm" -Force -ErrorAction SilentlyContinue
